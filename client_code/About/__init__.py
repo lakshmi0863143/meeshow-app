c5c2cff6e74da83bfc8f930fe008e748ea36f3bf
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-10-16 11:13:55 +0000

    Created Login; Edited About, Borrow, FAQs, Home and 6 more

--- client_code/About/__init__.py
+++ client_code/About/__init__.py
@@ -7,3 +7,18 @@ class About(AboutTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def text_area_2_change(self, **event_args):
+    """This method is called when the text in this text area is edited"""
+
+  def text_area_3_change(self, **event_args):
+    """This method is called when the text in this text area is edited"""
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+
+    
+
+   
+
--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -7,3 +7,54 @@ class Home(HomeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('About')
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Borrow')
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('FAQs')
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Lender')
+
+  def link_5_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Online_chat')
+
+  def link_6_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('QuickFacts')
+
+  def link_7_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('UnderFeedback')
+
+  def link_8_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('working')
+
+  def link_9_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Login')
+
+  def text_area_1_change(self, **event_args):
+    """This method is called when the text in this text area is edited"""
+  
+
+
+
+
+
+
+
+
+
+
+ 
